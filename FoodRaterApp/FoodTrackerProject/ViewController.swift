248a86daca1269be733209c41f02cc367e06848b
Michael Barnes <m.brandon.barnes@gmail.com>
2021-02-05 11:17:51 -0600

    Build a Basic UI

---
+++ FoodRaterApp/FoodTrackerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerProject
+//
+//  Created by ASUMH COMPUTER on 2/4/21.
+//  Copyright © 2021 ASUMH COMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
