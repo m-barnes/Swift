47db514bbd3154196146b8588271181bf936f9f5
Michael Barnes <m.brandon.barnes@gmail.com>
2021-02-10 15:41:17 -0600

    Create a Table View

---
+++ FoodRaterApp/FoodTrackerProject/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FoodTrackerProject
+//
+//  Created by ASUMH COMPUTER on 2/10/21.
+//  Copyright © 2021 ASUMH COMPUTER. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK:Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
